0c69fda90d7f86790ba29231a78183876682677b
David Abigt <_github@m42.biz>
2015-08-29 16:35:45 -0500

    A basic bat file with instructions in it to edit for your info

---
+++ GetJournals.bat
@@ -0,0 +1,7 @@
+rem edit the line below replacing:
+rem YOUR_LOGIN with your login (email)
+rem YOUR_PASSOWRD with your fitbit password
+rem DATE_OF_FIRST_JOURNAL with date of oldest journal entry.
+rem as in java -jar target/FitbitReader.jar bob@gmail.com P@$$w0rd 8/1/2013
+
+java -jar target/FitbitReader.jar YOUR_LOGIN YOUR_PASSOWRD DATE_OF_FIRST_JOURNAL
